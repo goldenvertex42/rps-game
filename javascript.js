26734ebc55c15f61d70f7f9ffa49b4c96917c6ee
goldenvertex42 <fabled93@gmail.com>
2025-07-05 09:17:39 -0500

    Create function to play a round of RPS and call it; create variables for scores

--- javascript.js
+++ javascript.js
@@ -9,17 +9,39 @@ function getComputerChoice() {
     }
 }
 
-const pcchoice = getComputerChoice();
-
 function getHumanChoice() {
-    let userInput = prompt("Rock, Paper, or Scissors?");
-    if (userInput === "Rock" || userInput === "rock") {
-        return "Rock";
-    } else if (userInput === "Paper" || userInput === "paper") {
-        return "Paper";
-    } else if (userInput === "Scissors" || userInput === "scissors") {
-        return "Scissors";
+    let userInput = prompt("Rock, Paper, or Scissors?").toLowerCase();
+    if (userInput === "rock") {
+        return "rock";
+    } else if (userInput === "paper") {
+        return "paper";
+    } else if (userInput === "scissors") {
+        return "scissors";
     }
 }
 
-const humanchoice = getHumanChoice()
+let computerScore = 0
+let humanScore = 0
+
+function playRound(humanChoice, computerChoice) {
+
+    if (humanChoice === "rock" && computerChoice === "Scissors") {
+        console.log("You won! Rock beats Scissors!");
+    } else if (humanChoice === "paper" && computerChoice === "Rock") {
+        console.log("You won! Paper beats Rock!");
+    } else if (humanChoice === "scissors" && computerChoice === "Paper") {
+        console.log("You won! Scissors beats Paper!");
+    } else if (humanChoice === "rock" && computerChoice === "Paper") {
+        console.log("You lost! Paper beats Rock!");
+    } else if (humanChoice === "paper" && computerChoice === "Scissors") {
+        console.log("You lost! Scissors beats Paper!");
+    } else if (humanChoice === "scissors" && computerChoice === "Rock") {
+        console.log("You lost! Rock beats Scissors!");
+    } 
+    
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
