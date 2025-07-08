5630d225cbb9d30cfe754e4b31bc9788b28f92f7
goldenvertex42 <fabled93@gmail.com>
2025-07-07 22:50:16 -0500

    Call 5 playRounds and compute winner; rework variables within playRound function

--- javascript.js
+++ javascript.js
@@ -1,14 +1,15 @@
+// SET funtion that randomly generates a choice for the computer //
 function getComputerChoice() {
     const pcInput = Math.floor(Math.random() * 3);
     if (pcInput === 0) {
-        return "Rock";
+        return "rock";
     } else if (pcInput === 1) {
-        return "Paper";
+        return "paper";
     } else if (pcInput === 2) {
-        return "Scissors";
+        return "scissors";
     }
 }
-
+// SET function that prompts user to make a choice //
 function getHumanChoice() {
     let userInput = prompt("Rock, Paper, or Scissors?").toLowerCase();
     if (userInput === "rock") {
@@ -20,35 +21,58 @@ function getHumanChoice() {
     }
 }
 
+function playGame() {
+
 let computerScore = 0
 let humanScore = 0
 
-function playRound(humanChoice, computerChoice) {
-
-    if (humanChoice === "rock" && computerChoice === "Scissors") {
+function playRound() {
+    // SET variables to hold choices within scope of playRound function to obtain new choices //
+    const humanChoice = getHumanChoice();
+    const computerChoice = getComputerChoice();
+    
+    if (humanChoice === computerChoice) {
+        console.log(`You tied! You both picked ${humanChoice}!`);
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
         console.log("You won! Rock beats Scissors!"); 
         ++humanScore;
-    } else if (humanChoice === "paper" && computerChoice === "Rock") {
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
         console.log("You won! Paper beats Rock!");
         ++humanScore;
-    } else if (humanChoice === "scissors" && computerChoice === "Paper") {
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
         console.log("You won! Scissors beats Paper!");
         ++humanScore;
-    } else if (humanChoice === "rock" && computerChoice === "Paper") {
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
         console.log("You lost! Paper beats Rock!");
         ++computerScore; 
-    } else if (humanChoice === "paper" && computerChoice === "Scissors") {
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
         console.log("You lost! Scissors beats Paper!");
         ++computerScore;
-    } else if (humanChoice === "scissors" && computerChoice === "Rock") {
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
         console.log("You lost! Rock beats Scissors!");
         ++computerScore;
-    } 
+    } else {
+        console.log("Invalid choice. Please try again.");
+    }
     
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+// CALL 5 playRound functions to gather data needed for final evaluation
+playRound();
+playRound();
+playRound();
+playRound();
+playRound();
 
-playRound(humanSelection, computerSelection);
-console.log(humanScore, computerScore)
+//COMPUTE who the winner is
+//Display a message oriented to the user's final place
+if (humanScore > computerScore) {
+    console.log(`YOU WON! You beat the computer ${humanScore} - ${computerScore}`);
+} else if (computerScore > humanScore) {
+    console.log(`YOU LOST! The computer beat you ${computerScore} - ${humanScore}`);
+} else if (humanScore === computerScore) {
+    console.log(`DRAW!! You tied the computer ${humanScore} - ${computerScore}`);
+}
+}
+
+playGame();
